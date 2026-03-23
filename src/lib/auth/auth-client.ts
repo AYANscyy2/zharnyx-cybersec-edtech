968cc27cce9ca8eed8ee4be476ac1beea92ac5ba
AYANscyy2 <mayan6378@gmail.com>
2026-03-23 16:03:19 +0530

    feat: Configure `authClient` to use the `inferAdditionalFields` plugin.

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,7 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { Session, auth } from "./auth";
 
-export const authClient = createAuthClient<typeof auth>();
+export const authClient = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
+});
 
 export const { signIn, signUp, signOut, useSession } = authClient;
 
